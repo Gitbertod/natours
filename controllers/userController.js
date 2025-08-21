53d98c13512ad89114ff7ed8194aa123c7b30307
Gilberto Díaz <diazp.gilberto@gmail.com>
2025-08-21 18:26:41 -0500

    se creo endpoint solo para editar nombre y correo de usuario existente

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const catchAsync = require("../utils/catchAsync");
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const AppError = require("../utils/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {}
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el]
+    })
+    return newObj
+}
 
 
 exports.getAllUsers = catchAsync(async (req, res) => {
@@ -11,10 +20,30 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     });
 })
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
     // 1) Create error if user posts password data
-    
-}
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route is not for password updates. Please use /updateMyPassword.', 400))
+    }
+    /*
+       2) Se filtran los  campos name e email para que no se pueda cambiar el rol del usuario a la ligera
+    */
+    const filteredBody = filterObj(req.body, 'name', 'email');
+
+    // 3) Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    });
+
+});
 
 exports.getUser = (req, res) => {
     res.status(500).json({
