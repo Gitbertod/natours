e345cde76e5360b134082eed9972faa9ac5b1599
Gilberto Díaz <diazp.gilberto@gmail.com>
2025-08-21 19:54:59 -0500

    se implemento limitador de solicitudes con middleware en app.js

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const rateLimit = require('express-rate-limit')
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController')
+const globalErrorHandler = require('./controllers/errorController');
+
+const app = express();
 
 
-//MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max:100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour'
+});
+
+app.use('/api',limiter)
+
 app.use(express.json()); //esto es un middleware
 
 app.use((req, res, next) => {
