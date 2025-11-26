a2c4c76d373e7fd51043af4304f024e782229992
Gilberto Díaz <diazp.gilberto@gmail.com>
2025-11-26 11:00:56 -0500

    se agrego enpoint /me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,18 +5,29 @@ const authController = require('./../controllers/authController')
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+//Protege todas las rutas despues de este middleware 
+router.use(authController.protect)
+
 router.patch(
     '/updateMyPassword',
-    authController.protect,
     authController.updatePassword
 );
-router.get("/me",authController.protect,userController.getMe,userController.getUser)
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe)
+router.get("/me",
+    userController.getMe,
+    userController.getUser
+)
+router.patch('/updateMe',
+    userController.updateMe
+);
+router.delete('/deleteMe',
+    userController.deleteMe
+)
+
+//de aqui para abajo solo son acciones que pueden hacer los admin
+router.use(authController.restrictTo('admin'))
 
 router.route("/")
     .get(userController.getAllUsers)
